5696f520964661f9d64896131fd59943c21383a4
cevher kemal sirin <76917072+cevherkemalsirin@users.noreply.github.com>
2023-12-30 05:14:00 +0300

    Create Initial Painting functionality
    Created mouse click events to paint the canvas with holding down the mouse.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 "use strict";
 
-let grid = 16;
-const panel = document.querySelector(".main");
+let grid = 100;
+let colorRGB = "rgb(241, 241, 241)";
+let canPaint = false;
+const canvas = document.querySelector(".main");
 
 
 
@@ -12,13 +14,46 @@ for(let r = 0; r<grid; r++)
 
     for(let col = 0; col <grid; col++)
     {
-        const cube = document.createElement("div");
-        cube.classList.add("cube");
-        row.append(cube);
+        const square = document.createElement("div");
+        square.classList.add("square");
+        row.append(square);
     }
-    panel.append(row);
+    canvas.append(row);
 }
 
-   
+
+function Paint(e)
+{
+    e.target.style.backgroundColor = "yellow";
+}
 
 
+document.addEventListener("mousedown", (e)=>
+{
+    canPaint = true;
+    if(e.target.classList.contains("square"))
+    {
+        Paint(e);
+    }
+});
+document.addEventListener("mouseup", (e)=>
+{
+    canPaint = false;
+});
+
+canvas.addEventListener("mouseover", (e)=> {
+    if(e.target.classList.contains("square") && canPaint)
+    {
+        Paint(e);
+    }
+});
+
+
+
+/*
+const pixels = document.querySelectorAll(".square");
+
+pixels.forEach( pixel => {
+    pixel.addEventListener("mouseover", Paint);
+});
+*/
