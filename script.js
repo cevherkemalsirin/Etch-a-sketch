53b1b5b52a69d7ea0b2d53fa09130f278f68c9e9
cevher kemal sirin <76917072+cevherkemalsirin@users.noreply.github.com>
2023-12-31 15:54:52 +0300

    Add left panel and Clear Function
    In order to pick custom color to paint the canvas. Added color picker with left panel. Also added clear button to erase the any painting on the canvas.

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 "use strict";
 
-let grid = 100;
-let colorRGB = "rgb(241, 241, 241)";
+let grid = 64;
+let customBackgroundColor = "rgb(255,255,255)";
 let canPaint = false;
-const canvas = document.querySelector(".main");
+const canvas = document.querySelector(".canvas");
+const colorPicker = document.querySelector(".colorPicker");
+const clearBtn = document.querySelector(".clearBtn");
+let pixels = null;
 
-
-
-for(let r = 0; r<grid; r++)
+function CreateCanvas()
 {
-    const row = document.createElement("div");
-    row.classList.add("tray");
-
-    for(let col = 0; col <grid; col++)
+    for(let r = 0; r<grid; r++)
     {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        row.append(square);
+        const row = document.createElement("div");
+        row.classList.add("tray");
+    
+        for(let col = 0; col <grid; col++)
+        {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            row.append(square);
+        }
+        canvas.append(row);
+        
     }
-    canvas.append(row);
+    pixels = document.querySelectorAll(".square");
 }
 
 
-function Paint(e)
+function Paint(e,color)
 {
-    e.target.style.backgroundColor = "yellow";
+    e.style.backgroundColor = color;
 }
 
 
@@ -33,7 +39,7 @@ document.addEventListener("mousedown", (e)=>
     canPaint = true;
     if(e.target.classList.contains("square"))
     {
-        Paint(e);
+        Paint(e.target,colorPicker.value);
     }
 });
 document.addEventListener("mouseup", (e)=>
@@ -44,10 +50,24 @@ document.addEventListener("mouseup", (e)=>
 canvas.addEventListener("mouseover", (e)=> {
     if(e.target.classList.contains("square") && canPaint)
     {
-        Paint(e);
+        Paint(e.target,colorPicker.value);
     }
 });
 
+
+function ClearCanvas()
+{
+    if(pixels)
+    {
+        pixels.forEach(pixel => {
+            Paint(pixel,customBackgroundColor);
+        });
+    }
+}
+
+clearBtn.addEventListener("click", ClearCanvas);
+
+CreateCanvas();
 
 
 /*
