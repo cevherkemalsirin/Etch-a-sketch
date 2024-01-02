86981e7ea80441d8ed0aaccd72ee3f0b69ed092c
cevher kemal sirin <76917072+cevherkemalsirin@users.noreply.github.com>
2024-01-02 21:04:35 +0300

    Created Brush types  and left panel
    Left panel including different type of  brush options added. First example is rainbow brush.

--- script.js
+++ script.js
@@ -3,11 +3,24 @@
 let grid = 64;
 let customBackgroundColor = "rgb(255,255,255)";
 let canPaint = false;
+
+
 const canvas = document.querySelector(".canvas");
 const colorPicker = document.querySelector(".colorPicker");
 const clearBtn = document.querySelector(".clearBtn");
+const rainbowBtn = document.querySelector(".rainbowBtn");
 let pixels = null;
 
+
+
+
+//enum 
+const brushType = {
+    rainbow : "rainbow",
+    solid : "solid",
+    darken: "darken"
+}
+let currentBrush = brushType.solid;
 function CreateCanvas()
 {
     for(let r = 0; r<grid; r++)
@@ -34,14 +47,45 @@ function Paint(e,color)
 }
 
 
+function GetRandomColor()
+{
+    return `rgb(${Math.trunc(Math.random() * 255)+1},${Math.trunc(Math.random() * 255)+1},${Math.trunc(Math.random() * 255)+1})`;
+}
+
+function DetermineColor()
+{
+    switch(currentBrush)
+    {
+        case brushType.solid : 
+        {
+            return colorPicker.value;
+        }break;
+        case brushType.rainbow :
+        {
+            return GetRandomColor();
+        }break;
+    }
+}
+
+colorPicker.addEventListener("click", ()=>
+{
+    currentBrush = brushType.solid;
+});
+
+rainbowBtn.addEventListener("click", ()=> 
+{
+    currentBrush = brushType.rainbow;
+});
+
 document.addEventListener("mousedown", (e)=>
 {
     canPaint = true;
     if(e.target.classList.contains("square"))
     {
-        Paint(e.target,colorPicker.value);
+        Paint(e.target, DetermineColor());
     }
 });
+
 document.addEventListener("mouseup", (e)=>
 {
     canPaint = false;
@@ -50,7 +94,9 @@ document.addEventListener("mouseup", (e)=>
 canvas.addEventListener("mouseover", (e)=> {
     if(e.target.classList.contains("square") && canPaint)
     {
-        Paint(e.target,colorPicker.value);
+        let a = DetermineColor();
+        console.log(a);
+        Paint(e.target,a);
     }
 });
 
